572a2f85a9aae2d7dc4daec1e57e1002c610e551
Shreya Tiwari <shreyatiwari0801@gmail.com>
2026-07-01 11:05:26 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost"
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "") 
