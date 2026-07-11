aec47513bec69a0eb2168fe162c63a1c7c5fd3a5
Shreya Tiwari <shreyatiwari0801@gmail.com>
2026-07-11 22:31:59 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -17,6 +17,7 @@ print("MYSQL_USER =", MYSQL_USER)
 print("MYSQL_PASSWORD =", repr(MYSQL_PASSWORD)) 
 print("MYSQL_DATABASE =", MYSQL_DATABASE)
 
+
 def _connect(use_database=True):
     config = {
         "host": MYSQL_HOST,
