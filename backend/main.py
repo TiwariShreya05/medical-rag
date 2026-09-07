e74ea3168fd095ae84e6f8bfe8974de7f34cbc82
Shreya Tiwari <shreyatiwari0801@gmail.com>
2026-09-07 10:26:36 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import Optional
+
 import json 
 import logging 
 import os
